f5b88e2605e1ee68fd03b66397535389546d576b
voidfer <116502166+voidfer@users.noreply.github.com>
2026-03-12 03:47:04 +0300

    Add a new migration for email confirmation internally

---
+++ zero2prod/migrations/20260312004153_add_status_and_subscription_tokens.sql
@@ -0,0 +1,11 @@
+-- Add migration script here
+
+-- Add subscriber status 
+ALTER TABLE subscriptions
+ADD COLUMN status TEXT NOT NULL DEFAULT 'pending_confirmation'
+
+-- Table for confirmation tokens 
+CREATE TABLE subscription_tokens (
+	subscription_token TEXT PRIMARY KEY,
+	subscriber_id  UUID NOT NULL REFERENCES subscriptions(id)
+);
